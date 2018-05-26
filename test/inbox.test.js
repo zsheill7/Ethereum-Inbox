522b40a36ef0fc4138ab2bd6012968c966d14862
Zoe Sheill <zoe@Zoes-MacBook-Pro.local>
2018-05-26 10:13:32 -0700

    initial mocha tests

--- test/inbox.test.js
+++ test/inbox.test.js
@@ -3,22 +3,49 @@ const ganache = require('ganache-cli');
 const Web3 = require('web3');
 
 const web3 = new Web3(ganache.provider());
+const { interface, bytecode } = require('../compile');
 
-class Car {
-  park() {
-    return 'stopped';
-  }
+let accounts;
+let inbox;
 
-  drive() {
-    return 'vroom';
-  }
-}
+beforeEach(async () => {
+  // Get list of accounts
+  accounts = await web3.eth.getAccounts();
 
+  // Use one account to deploy contract
+  inbox = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({ data: bytecode, arguments: ['Yo mah boi']})
+    .send({ from: accounts[0], gas: '1000000'});
+});
 
-
-describe('Car', () => {
-  it('it can park', () => {
-    const car = new Car();
-    assert.equal(car.park(), 'stopped');
+describe('Inbox', () => {
+  it('deploys a contract', () => {
+    assert.ok(inbox.options.address);
   });
-})
+});
+
+// class Car {
+//   park() {
+//     return 'stopped';
+//   }
+//
+//   drive() {
+//     return 'vroom';
+//   }
+// }
+//
+// let car;
+//
+// beforeEach(() => {
+//   car = new Car();
+// });
+//
+// describe('Car', () => {
+//   it('it can park', () => {
+//     assert.equal(car.park(), 'stopped');
+//   });
+//
+//   it('it can drive', () => {
+//     assert.equal(car.drive(), 'vroom');
+//   });
+// })
