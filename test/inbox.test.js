52ea38064310ed4a1587ae011761e36517635ebf
Zoe Sheill <zoe@Zoes-MacBook-Pro.local>
2018-05-26 18:07:28 -0700

    updated deploy

--- test/inbox.test.js
+++ test/inbox.test.js
@@ -8,19 +8,33 @@ const { interface, bytecode } = require('../compile');
 let accounts;
 let inbox;
 
+let MAH_BOI = 'Yo mah boi';
+
 beforeEach(async () => {
   // Get list of accounts
   accounts = await web3.eth.getAccounts();
 
   // Use one account to deploy contract
   inbox = await new web3.eth.Contract(JSON.parse(interface))
-    .deploy({ data: bytecode, arguments: ['Yo mah boi']})
+    .deploy({ data: bytecode, arguments: [MAH_BOI]})
     .send({ from: accounts[0], gas: '1000000'});
 });
 
 describe('Inbox', () => {
   it('deploys a contract', () => {
     assert.ok(inbox.options.address);
+  });
+
+  it('has a default message', async () => {
+    const message = await inbox.methods.message().call();
+    assert.equal(message, MAH_BOI);
+  });
+
+  it('can change the message', async () => {
+    await inbox.methods.setMessage('bye')
+      .send({ from: accounts[0]});
+    const message = await inbox.methods.message().call();
+    assert.equal(message, 'bye');
   });
 });
 
